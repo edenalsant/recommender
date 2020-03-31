f4a6a32b200ca295d1dc03a19d82a15af5a8df19
= <eden.ernandes@inloco.com.br>
2020-03-31 12:12:18 -0300

    refactor(tfidf): add methods to get similiar movies

--- data/tfidf.py
+++ data/tfidf.py
@@ -1,13 +1,37 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
-def remove_stop_words(lang):
-     return TfidfVectorizer(stop_words=lang)
+import pandas as pd
 
-def replace_nan(dataset, col, string):
-    dataset[col] = dataset[col].fillna(string)
+def generate_tfidf_matrix(tfidf, dataset, col):
+    return tfidf.fit_transform(dataset['overview'])
 
-def build_matrix(dataset, col):
-    tfidf = remove_stop_words('english')
-    replace_nan(dataset, col, '')
-    return tfidf.fit_transform(dataset[col])
+def generate_movies_indices(dataset, col):
+    return pd.Series(dataset.index, index=dataset[col]).drop_duplicates()
 
+def get_similar_movies_indeces(cosine_sim, index, amount):
+    similar_score = list(enumerate(cosine_sim[index]))
+    similar_score = sorted(similar_score, key=lambda x: x[1], reverse=True)
+    last_movie_index = amount + 1
+    return similar_score[1:last_movie_index]
+
+def clear_dataset(dataset, col):
+    return dataset[col].fillna('')
+
+def get_similar_movies(dataset, movie_name, number_related_movies):
+    tfidf = TfidfVectorizer(stop_words='english')
+    
+    dataset['overview'] = clear_dataset(dataset, 'overview')
+
+    tfidf_matrix = generate_tfidf_matrix(tfidf, dataset, 'overview')
+
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    indices = generate_movies_indices(dataset, 'title')
+
+    movie_index = indices[movie_name]
+    
+    similar_score = get_similar_movies_indeces(cosine_sim, movie_index, number_related_movies)
+
+    movie_indices = [i[0] for i in similar_score]
+    return dataset['title'].iloc[movie_indices]
