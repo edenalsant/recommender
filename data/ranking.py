eb1dd20e49004ee9dd926a7810b9d06e540a1f73
= <eden.ernandes@inloco.com.br>
2020-03-31 12:07:47 -0300

    feat(ranking): add wrapper method

--- data/ranking.py
+++ data/ranking.py
@@ -7,7 +7,7 @@ def get_minmum_votes_needed(dataset, amount):
     return dataset['vote_count'].quantile(amount)
 
 def get_eligible_movies(dataset, minimum_voted_needed):
-    return dataset.copy().loc[dataset['vote_count'] >= minimum_voted_needed]
+    return dataset.loc[dataset['vote_count'] >= minimum_voted_needed]
 
 def get_weighted_rating(eligible_movies, minimum_voted_needed, general_mean):
     v = eligible_movies['vote_count']
@@ -17,3 +17,15 @@ def get_weighted_rating(eligible_movies, minimum_voted_needed, general_mean):
 def add_colum_to_dataset(dataset, column, weighted_rating):
     dataset[column] = dataset.apply(weighted_rating, axis=1)
     return dataset
+
+def rank_movies(dataset):
+    general_mean = get_mean(dataset, "vote_average")
+    minimum_votes_needed = get_minmum_votes_needed(dataset, 0.9)
+
+    dataset = get_eligible_movies(dataset, minimum_votes_needed)
+
+    dataset['score'] = get_weighted_rating(dataset, minimum_votes_needed, general_mean)
+    
+    dataset = dataset.sort_values('score', ascending=False)
+
+    return dataset.reset_index()
