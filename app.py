0be663d0dde68fe18f8f841e64b8fd04491ca97a
= <eden.ernandes@inloco.com.br>
2020-03-31 12:17:34 -0300

    feat(app): add callers to algorithms

--- app.py
+++ app.py
@@ -2,20 +2,18 @@ from data import loader
 from data import ranking
 from data import tfidf
 
+def get_movies_ranking(dataset):
+    return ranking.rank_movies(dataset)
+
+def get_recomendations(dataset, movie_name, number_related_movies):
+    return tfidf.get_similar_movies(dataset, movie_name, number_related_movies) 
+
 def run():
     movies_dataset = loader.load_dataset('./resources/movies_metadata.csv')
-    
-    general_mean = ranking.get_mean(movies_dataset, "vote_average")
-    minimum_votes_needed = ranking.get_minmum_votes_needed(movies_dataset, 0.9)
+    movies_dataset = get_movies_ranking(movies_dataset)
+    print('these are the top 20 movies: ')
+    print(movies_dataset['title'].head(20))
 
-    eligible_movies = ranking.get_eligible_movies(movies_dataset, minimum_votes_needed)
-
-    weighted_rating = ranking.get_weighted_rating(eligible_movies, minimum_votes_needed, general_mean)
-    
-    eligible_movies['score'] = weighted_rating
-    eligible_movies = eligible_movies.sort_values('score', ascending=False)
-
-    tfidf_matrix = tfidf.build_matrix(eligible_movies, 'overview')
-    print(tfidf_matrix.shape)
-
-
+    movie_name = input('movie title: ')
+    number_of_movies = int(input('how many movies? '))
+    print(get_recomendations(movies_dataset, movie_name, number_of_movies))
